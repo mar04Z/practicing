0c70a9438af4bc009c7494232761cc8ae319a365
Marjorie <geraldineumasicoaguila@gmail.com>
2023-05-07 12:29:04 -0500

    bienvenida al curso

--- ejer1.java
+++ ejer1.java
@@ -2,5 +2,6 @@ public class ejer1 {
     public static void main(String args[]) {
         System.out.println("Hola Mundo");
         System.out.println("Mi nombre es Geraldine");
+        System.out.println("Bienvenida al curso de EDA");
     }
 }
